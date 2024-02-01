f29fe69ef2e70723273c4d3258ae80b36b633995
Qofimain <xcomdaewi@gmail.com>
2024-02-01 08:31:39 +0000

    01/02/2024

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
 	import '../app.pcss';
 </script>
+<nav>
+	<a href="/">Home</a>
+	<a href="/contacts">contacts</a>
+</nav>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev!</a> To read the documentation</p>
-<button class="btn">chek</button>
+<a class="btn" href= "https://5173-qofimain-sveltekit-eod8yqxdpxs.ws-eu107.gitpod.io/contacts">contacts</a>
+
