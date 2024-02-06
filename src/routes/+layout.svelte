cc389111d2101180f5642bd2165d9813adf5bcfd
Qofimain <xcomdaewi@gmail.com>
2024-02-06 06:06:40 +0000

    python

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
+<script>
+	import '../app.pcss';
+</script>
+
 <nav>
 	<a href="/">home</a>
 	<a href="/blog">blog</a>
+	<a href="/python">Python</a>
 </nav>
 
 <slot />
--- src/routes/blog/[slug]/+page.server.js
+++ src/routes/blog/[slug]/+page.server.js
@@ -1,9 +1,8 @@
 import { error } from '@sveltejs/kit';
 import { posts } from '$lib/datas/data.js';
 
-export function load(obj) {
-	const post = posts.find((post) => post.slug === obj.params.slug2);
-    console.log(obj)
+export function load({ params }) {
+	const post = posts.find((post) => post.slug === params.slug);
 	if (!post) throw error(404);
 
 	return {
