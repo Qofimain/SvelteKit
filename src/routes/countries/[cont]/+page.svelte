9f13c83fe78df8931b00c19248b5a663df8bc330
Qofimain <xcomdaewi@gmail.com>
2024-02-14 06:47:47 +0000

    styleWhite

--- src/routes/countries/[cont]/+page.svelte
+++ src/routes/countries/[cont]/+page.svelte
@@ -8,7 +8,7 @@
 	<title>{$page.params.cont}</title>
 </svelte:head>
 
-<div style="border:1px solid blue;padding:25px">
+<div style="border:5px solid white;padding:20px">
 	{#each data.world.alldata.filter((country) => country[1] == $page.params.cont.toUpperCase()) as item}
 		<p>{item[0]}</p>
 	{/each}
