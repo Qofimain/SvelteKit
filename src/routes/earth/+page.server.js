20a7bebafb293977774871a7a80caa95b5e3b1c1
Qofimain <102502529+Qofimain@users.noreply.github.com>
2024-02-26 10:52:45 +0500

    Update +page.server.js

--- src/routes/earth/+page.server.js
+++ src/routes/earth/+page.server.js
@@ -4,14 +4,28 @@ const py = api.interpreter;
 let pymodule = py.importSync("src/lib/py/pydata.py");
 // pymodule = py.importSync("$lib/py/pydata.py", true);
 
-
 	export  function load({ params, cookies }) {
+		
+		const cookiesCount = cookies.get("cookiesCount")
+
+		if(Number(cookiesCount) > 0){
+			cookies.set("cookiesCount", (Number(cookiesCount)+1), {path:'/'})	
+		}
+		else{
+			cookies.set("cookiesCount", 1, {path:'/'})
+		}
+		
+
+
 		const visited = cookies.get("visited")
-		console.log(visited)
+		
 
 		const result2 = py.evalSync(pymodule, "getcountries()");
+		const result1 = cookiesCount
 		result2.visited = visited
         console.log(result2)
+		console.log(result1)
+		
 		return result2;
 
 	}	
